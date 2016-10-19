3c5c557f0070437f19541290386b62b47c7aab2e
Carsten Börnert <carsten.bornert@uk.ibm.com>
2016-10-19 21:43:29 +0200

    changed qs.clear

--- 99_docker/mq-sc2/testscripts/qs-clear.sh
+++ 99_docker/mq-sc2/testscripts/qs-clear.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 echo "Starting to clear queues..."
+echo "SC2.TEST.CAMPAIGN"
+amqsget SC2.TEST.CAMPAIGN
 echo
-echo "clear ql (SC2.TEST.CAMPAIGN)" | runmqsc >/dev/null
-echo "clear ql (SC2.TEST.CANCEL)" | runmqsc >/dev/null
-echo "clear ql (SC2.TEST.FILTERED)" | runmqsc >/dev/null
-echo "clear ql (EVENT.ORDER)" | runmqsc >/dev/null
+echo
+echo "SC2.TEST.CANCEL"
+amqsget SC2.TEST.CANCEL
+echo
+echo
+echo "SC2.TEST.FILTERED"
+amqsget SC2.TEST.FILTERED
+echo
+echo
+echo "EVENT.ORDER"
+amqsget EVENT.ORDER
+echo
 echo "Done."
 echo
+
