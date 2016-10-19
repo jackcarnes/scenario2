1850ade57bbb11ced173163daaf41bad85f5a743
Carsten Börnert <carsten.bornert@uk.ibm.com>
2016-10-19 11:25:55 -0700

    more bug fixes

--- 99_docker/mq-sc2/testscripts/configure-stubfiles.sh
+++ 99_docker/mq-sc2/testscripts/configure-stubfiles.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
- curl -H "Content-Type: application/json" -X POST -d '{"orderId":"1","accountName":"'$1'","salesforceId":"'$2'"}' http://stub:7800/updatefile
+echo
+echo "Updating order 1 with accountName $1 and salesforceId $2"
+echo
+curl -H "Content-Type: application/json" -X POST -d '{"orderId":"1","accountName":"'$1'","salesforceId":"'$2'"}' http://stub:7800/updatefile
+echo
+echo
+echo "Updating order 2 with accountName $1 and salesforceId $2"
+echo
+curl -H "Content-Type: application/json" -X POST -d '{"orderId":"2","accountName":"'$1'","salesforceId":"'$2'"}' http://stub:7800/updatefile
+echo
+echo "Done"
+echo
